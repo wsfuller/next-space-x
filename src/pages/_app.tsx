2aee64a6e33fa5240c8a41223b247075f12049c8
Steve Fuller <wsfuller.dev@gmail.com>
2022-07-07 07:38:34 -0700

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp
